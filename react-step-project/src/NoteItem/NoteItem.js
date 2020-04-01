fae4211bc57081954c0ccf0e733deb7b96f44fd9
narmincebiyeva <narmincebiyeva@gmail.com>
2020-04-01 12:45:57 +0400

    note item

---
+++ react-step-project/src/NoteItem/NoteItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class NoteItem extends Component {
+    render() {
+        return (
+            <div>
+                Note items
+            </div>
+        );
+    }
+}
+
+export default NoteItem;
